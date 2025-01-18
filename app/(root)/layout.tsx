e84ad53b7183ed11a41bf594b623e9b79323d254
jacklvd <vodanglong3103@gmail.com>
2025-01-18 15:03:48 -0500

    set-up

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Header from "@/components/Header";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+    return (
+        <main className="root-container">
+            <div className="mx-auto max-w-7x">
+                <Header />
+                <div className="mt-20 pb-20">
+                    {children}
+                </div>
+            </div>
+        </main>
+
+    )
+}
+
+export default Layout;
